7e7437a67163ddb6645efbbb721ab78a3b0876af
Kevin <thekevinjung@gmail.com>
2021-04-10 00:23:50 -0400

    functioning calculator and updated colors

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function operate(operator, num1, num2){
     }
 }
 
-const displayValue = document.querySelector(".display-value")
+const displayValue = document.getElementById("display")
 const numberButtons = document.querySelectorAll("input") //selects all the input elements and puts into an array
 const operatorButtons = document.querySelectorAll(".operator") // selects all the elements with class of operator
 const equalButton = document.getElementById("equal")
@@ -55,30 +55,35 @@ const equalButton = document.getElementById("equal")
 numberButtons.forEach(number => { //loops over the array and for each input value
     number.addEventListener("click", function(e){ //add an event listener of "click" which will run the function
         if(number.value === "."){
-            displayValue.textContent += "." //adds a decimal 
+            if(displayValue.textContent.indexOf(".") === -1){ //checks if the number has a decimal in it, if it does then do nothing
+                displayValue.textContent += "." //if there is no decimal added yet, then it adds a decimal 
+            }
         } else if(number.value === "<--"){
             displayValue.textContent = displayValue.textContent.slice(0,-1) //removes the last digit 
         } else if(number.value === "AC"){
             displayValue.textContent = "0" //clears the display back to 0
+            firstNum = ""
+            secondNum = ""
         } else if(number.value === "%"){
             displayValue.textContent = (displayValue.textContent)/100 //turns number into a percentage
         } else if (number.value === "="){
-            secondNum = displayValue.textContent.slice(firstNum.length + 1) // second number is the number that is after the operator
+            secondNum = displayValue.textContent // second number is the number that is after the operator
             displayValue.textContent = operate(operator, parseInt(firstNum), parseInt(secondNum)) //performs the calculation
         }else if (number.className === "operator"){
             firstNum = displayValue.textContent
+
             if(number.value === "/"){
                 operator = "divide"
-                displayValue.textContent += "/"
+                displayValue.textContent = ""
             } else if (number.value === "-"){
                 operator = "subtract"
-                displayValue.textContent += "-"
-            } else if (number.value === "*"){
+                displayValue.textContent = ""
+            } else if (number.value === "x"){
                 operator = "multiply"
-                displayValue.textContent += "*"
+                displayValue.textContent = ""
             } else {
                 operator = "add"
-                displayValue.textContent += "+"
+                displayValue.textContent = ""
             }
         } else {
             if(displayValue.textContent === "0"){ //the calculator's display will start at 0
@@ -91,6 +96,20 @@ numberButtons.forEach(number => { //loops over the array and for each input valu
     });
 });
 
+// keyboard support for entering numbers and backspace function
+window.addEventListener("keydown", function(e){
+    if(e.key <= 9){
+        if(displayValue.textContent === "0"){ //the calculator's display will start at 0
+            displayValue.textContent = e.key; //then it will become whatever number is clicked
+        } else {
+            displayValue.textContent += e.key; //afterwards it will join other numbers behind it
+        }
+    } else if(e.key === "Backspace") {
+        displayValue.textContent = 0
+    } else if(e.key === "Enter"){
+        displayValue.textContent = operate(operator, parseInt(firstNum), parseInt(secondNum))
+    }
+})
 
 //enter first number into calc
 //clear the display when the operator button is clicked
